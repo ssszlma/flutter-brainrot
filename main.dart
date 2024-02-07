44a2764abd58288c60c5e12def306f222f865fcc
Salma J <87875257+sa1l1@users.noreply.github.com>
2024-02-07 18:49:00 -0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,22 +1,227 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:owo/views/home_page.dart';
-
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'owo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'owo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: MyHomePage(),
       ),
-      home: const HomePage(),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair("Welcome, ", "Choose your path");
+
+  void getNext() {
+    current = WordPair("Welcome to ST, ", "Choose your path");
+    notifyListeners();
+  }
+
+  void choice1() {
+    current = WordPair("You died: ", "imagine i felt bothered enough to write this");
+    notifyListeners();
+  }
+  void choice2() {
+    current = WordPair("You died: ", "the UI is so ugly");
+    notifyListeners();
+  }
+  void choice3() {
+    current = WordPair("I swear I'm not dumb", "Just busy, that's why this took forever");
+    notifyListeners();
+  }
+
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+      case 1:
+        page = HomePage();
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: constraints.maxWidth >= 600,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.message),
+                    label: Text('Channels'),
+                  ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
+class GeneratorPage extends StatefulWidget {
+  @override
+  _GeneratorPageState createState() => _GeneratorPageState();
+}
+
+class _GeneratorPageState extends State<GeneratorPage> {
+  bool buttonsVisible = true;
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          if (buttonsVisible)
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      buttonsVisible = false;
+                    });
+                    appState.choice1();
+                  },
+                  child: Text('#lobby'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      buttonsVisible = false;
+                    });
+                    appState.choice2();
+                  },
+                  child: Text('#discussion'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      buttonsVisible = false;
+                    });
+                    appState.choice3();
+                  },
+                  child: Text('#questions'),
+                ),
+              ],
+            ),
+          if (!buttonsVisible)
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  buttonsVisible = true;
+                });
+                appState.getNext();
+              },
+              child: Text('Return'),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatefulWidget {
+  final WordPair pair;
+
+  const BigCard({Key? key, required this.pair}) : super(key: key);
+
+  @override
+  _BigCardState createState() => _BigCardState();
+}
+
+class _BigCardState extends State<BigCard> {
+  bool textClicked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          textClicked = !textClicked;
+        });
+      },
+      child: Card(
+        color: theme.colorScheme.primary,
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                widget.pair.first,
+                style: style,
+              ),
+              SizedBox(height: 8),
+              Text(
+                widget.pair.second,
+                style: TextStyle(fontSize: 25.0,  color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
